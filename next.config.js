9efc7ba2a5b8227f974a3be98ed75cde4667ddfa
White Dog <dlrlgus1204@naver.com>
2022-02-23 16:59:06 +0900

    NextJS redirect & rewrite

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 const API_KEY = process.env.API_KEY;
 
+// redirect를 이용해서 해당 페이지 주소에서 다른 페이지 주소로 이동하게 할 수 있음 ( 아예 다른 URL의 웹사이트로 이동하게 할 수도 있음 )
+// rewrites는 유저를 redirect 시키기는 하지만 URL은 변하지 않음
 module.exports = {
   reactStrictMode: true,
   async redirects() {
     return [
       {
-        source: "/contact",
-        destination: "/form",
+        source: "/contact/:path*",
+        destination: "/",
         permanent: false,
       },
     ]
